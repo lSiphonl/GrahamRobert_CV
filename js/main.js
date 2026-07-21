3737f955af76420a34867f0003b6a45c88a4cef4
Graham Robert <grahamrobert2004@gmail.com>
2026-07-21 13:18:21 +0200

    Added landing page css | Added javascript to keep right side scrollable, and left static

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("mousemove", (event) => {
+    document.body.style.setProperty("--x", `${event.clientX}px`);
+    document.body.style.setProperty(
+        "--y",
+        `${event.clientY + window.scrollY}px`
+    );
+});
